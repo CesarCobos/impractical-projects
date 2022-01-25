6d2210722da8197a2025df5c1e0a1f4e4c9333ec
cesar cobos <cesarcoboscv@gmail.com>
2022-01-25 09:05:40 -0600

    PEP 8 changes

--- lyrics_generator.py
+++ lyrics_generator.py
@@ -12,55 +12,35 @@
 
 import random
 
-subject =[
-    'mami','chica','bebé',
-    'princess' , 'lady'
-    ]
+subject = ['mami','chica','bebé','princess','lady']
+action = ['quiero','vamos a','voy a','vengo a']
+verb = ['darte','castigarte','encenderte','amarte','ligar','jugar']
+intensity = ['duro', 'rapido','lento', 'suave','fuerte']
+time = ['hasta que salga el sol','toda la noche','hasta el amanecer',
+    'hasta mañana','todo el día'] 
+something = ['sin miedo','sin anestesia','contra el piso',
+    'contra la pared','sin compromiso','face to face','sin compromiso']
 
-action=[
-    'quiero','vamos a','voy a', 'vengo a'
-    ]
-verb=[
-    'darte','castigarte','encenderte','amarte','ligar', 'jugar'
-    ]
-intensidad=[
-    'duro', 'rapido','lento', 'suave','fuerte'
-    ]
-time=[
-    'hasta que salga el sol','toda la noche', 'hasta el amanecer',
-    'hasta mañana', 'todo el día'] 
-something=[
-    'sin miedo', 'sin anestesia', 'contra el piso',
-    'contra la pared', 'sin compromiso', 'face to face',
-    'sin compromiso'
-    ]
 
-def run():  
-
+def run():
     #Opening txt file
-    f = open("lyrics.txt", "w")
-    
+    f = open("lyrics.txt","w")
     #Here starts to generate lyrics and writting the TXT file
-
     #intro: Using a for loop to create a kind of intro
-    f.write("\n[ Intro ] \n")
+    f.write("\n[ Intro ]\n")
     for i in range(4):
-        intro=(
-            random.choice(subject).title() 
+        intro = (random.choice(subject).title() 
             + " " 
             + random.choice(action) 
             + " " 
             + random.choice(verb) 
             + " " 
-            + random.choice(intensidad) 
+            + random.choice(intensity) 
             + " " 
-            + random.choice(time)
-            )
+            + random.choice(time))
         f.write(intro+"\n")
-
     #bridge: Just a pre-corus
-    bridge=(
-        "\n[ Bridge ]\n" 
+    bridge = ("\n[ Bridge ]\n" 
         + random.choice(verb).title() 
         + " " 
         + random.choice(verb) 
@@ -69,32 +49,26 @@ def run():
         + ", " 
         + random.choice(time) 
         + ", " 
-        + random.choice(intensidad) 
-        + "\n"
-        )
+        + random.choice(intensity) 
+        + "\n")
     f.write(bridge + "\n")
-
     #Corus: similar to "Intro", it is created with a for loop
     f.write("\n[ Coro ] \n")
     for i in range(4):
-        coro1=(
-            random.choice(subject).title()  
-            +" " 
+        coro1 = (random.choice(subject).title()  
+            + " " 
             + random.choice(action) 
             + " " 
             + random.choice(verb) 
             + " " 
-            + random.choice(intensidad) 
+            + random.choice(intensity) 
             + " " 
             + random.choice(time) 
             + " " 
-            + random.choice(something)
-            )
+            + random.choice(something))
         f.write(coro1 + "\n")
-
     #Another bridge
-    bridge2=(
-        "\n[ Bridge ]\n" 
+    bridge2 = ("\n[ Bridge ]\n" 
         + random.choice(verb).title() 
         + " " 
         + random.choice(verb) 
@@ -103,16 +77,13 @@ def run():
         + ", " 
         + random.choice(time) 
         + ", " 
-        + random.choice(intensidad) 
-        + "\n"
-        )
+        + random.choice(intensity) 
+        + "\n")
     f.write(bridge2)
-  
     #Now, lyrics are done, and we can close the TXT file
     f.close()
-
     #Now it is showing the file in the prompt
-    f = open("lyrics.txt", "r")
+    f = open("lyrics.txt","r")
     print(f.read())
     f.close()
 
--- qrCode.py
+++ qrCode.py
@@ -1,15 +1,14 @@
 import qrcode
 
-def run():
-    uInput=str(input("\nQué quieres inluir en tu codigo QR? \n"))
-    
-    img = qrcode.make(uInput)
 
-    f = open("qrcode.png", "wb")
+def run():
+    uInput = str(input("\nQué quieres inluir en tu codigo QR? \n"))
+    img = qrcode.make(uInput)
+    f = open("qrcode.png","wb")
     img.save(f)
     f.close()
+    print("\nTu codigo QR se ha creado!\n")
 
-    print("\n Tu codigo QR se ha creado! \n")
 
 if __name__ == '__main__':
     run()
